115418767793b7d4914ef4dae28741e85acc0eaf
Roman Zaichenko <romanzaichenko33@gmail.com>
2026-02-06 16:55:41 +0200

    Create node creation after lmb release when dragging edge.

--- src/app/map-area/page.tsx
+++ src/app/map-area/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import Zoomer from "./components/Zoomer";
-import { applyNodeChanges, Edge, NodeChange, ReactFlow, Node, EdgeChange, applyEdgeChanges, addEdge, Connection, Background , ConnectionMode, useReactFlow, ReactFlowProvider} from "@xyflow/react";
-import { useCallback, useState } from "react";
+import { applyNodeChanges, Edge, NodeChange, ReactFlow, Node, EdgeChange, applyEdgeChanges, addEdge, Connection, Background , ConnectionMode, useReactFlow, ReactFlowProvider, OnConnectStartParams} from "@xyflow/react";
+import { useCallback, useRef, useState } from "react";
 import '@xyflow/react/dist/style.css';
 import MapNode from "./components/MapNode";
 
@@ -19,6 +19,7 @@ function MapFlow() {
 
   const {screenToFlowPosition, getEdges} = useReactFlow()
   
+  const connectionStartRef = useRef<OnConnectStartParams>(null)
 
   const onNodesChange = useCallback( 
     (changes: NodeChange[]) => setNodes((nodesSnapshot) => 
@@ -76,6 +77,69 @@ function MapFlow() {
     [screenToFlowPosition, setNodes]
   )
 
+
+  const onConnectStart = useCallback((event : MouseEvent | TouchEvent,
+    params: OnConnectStartParams) => {
+    connectionStartRef.current = params
+  }, [])
+
+  const onConnectEnd = useCallback((event: MouseEvent | TouchEvent)=> {
+    const target = event.target as Element;
+    const isPane = target.classList.contains('react-flow__pane');
+    
+    if (isPane && connectionStartRef.current) {
+      const {clientX, clientY} = 'changedTouches' in event ? 
+        event.changedTouches[0] : event;
+
+      const position = screenToFlowPosition({
+        x: clientX,
+        y: clientY
+      })
+
+      const newNodeId = `node-${Date.now()}`;
+      const newNode : Node = {
+        id: newNodeId,
+        type: "mapNode",
+        position,
+        data: {label: "New Node", level: 2},
+        origin: [0.5, 0.5] as [number, number]
+      }
+
+      const {nodeId, handleType, handleId} = connectionStartRef.current
+      
+      const idTranslate: Record<string, string> = {
+        "rtc": "l",
+        "r": "l",
+        "rbc": "l",
+        "bml": "t",
+        "bmr": "t",
+        "b": "t",
+        "ltc": "r",
+        "l": "r",
+        "lbc": "r",
+        "tml": "b",
+        "tmr": "b",
+        "t": "b"
+      }
+
+      const endHandleId = idTranslate[handleId as string];
+
+
+
+      const newEdge : Edge = {
+        id: `e-${newNodeId}-${nodeId}`,
+        source: handleType === 'source' ? nodeId! : newNodeId,
+        target: handleType === 'source' ? newNodeId : nodeId!,
+        sourceHandle: handleType === 'source' ? handleId : endHandleId,
+        targetHandle: handleType === 'target' ? handleId : endHandleId,
+        type: 'bezier'
+      }
+
+      setNodes((nodes) => nodes.concat(newNode))
+      setEdges((edges) => edges.concat(newEdge))
+    }
+  
+  }, [screenToFlowPosition, setNodes, setEdges])
   return (
       <div className="w-[100vw] h-[90vh]">
         <ReactFlow 
@@ -91,6 +155,8 @@ function MapFlow() {
           connectionRadius={20}
           onPaneContextMenu={onPaneContextMenu}
           isValidConnection={isValidConnection}
+          onConnectStart={onConnectStart}
+          onConnectEnd={onConnectEnd}
           defaultEdgeOptions={{
             type: "bezier",
             
