c31fae9c87d04cc5a48ed1119a6eb980896554f9
Roman Zaichenko <romanzaichenko33@gmail.com>
2026-02-07 22:25:31 +0200

    Refactor MapFlow component.
    Create hook for map logic, in order to separate ui and business logic.

---
+++ src/app/map-area/hooks/useMapLogic.ts
@@ -0,0 +1,144 @@
+import { applyNodeChanges, Edge, NodeChange, ReactFlow, Node, EdgeChange, applyEdgeChanges, addEdge, Connection, Background , ConnectionMode, useReactFlow, ReactFlowProvider, OnConnectStartParams} from "@xyflow/react";
+import { useCallback, useRef, useState } from "react";
+import MapNode from "../components/MapNode";
+
+const initialNodes : Node[] = [];
+const initialEdges : Edge[] = [];
+
+const nodeTypes = {
+  mapNode: MapNode,
+}
+
+const idTranslate: Record<string, string> = {
+  "rtc": "l",
+  "r": "l",
+  "rbc": "l",
+  "bml": "t",
+  "bmr": "t",
+  "b": "t",
+  "ltc": "r",
+  "l": "r",
+  "lbc": "r",
+  "tml": "b",
+  "tmr": "b",
+  "t": "b"
+}
+
+export function useMapLogic() {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const {screenToFlowPosition, getEdges} = useReactFlow()
+  
+  const connectionStartRef = useRef<OnConnectStartParams>(null)
+
+  const onNodesChange = useCallback( 
+    (changes: NodeChange[]) => setNodes((nodesSnapshot) => 
+      applyNodeChanges(changes,nodesSnapshot)), []
+  );
+
+  const onEdgesChange = useCallback(
+    (changes:  EdgeChange[]) => setEdges((edgesSnapshot) =>
+      applyEdgeChanges(changes, edgesSnapshot)), []
+  );
+
+  const onConnect = useCallback(
+    (params: Edge | Connection) => setEdges((edgesSnapshot) => 
+      addEdge(params, edgesSnapshot)), []
+  );
+
+  const isValidConnection = useCallback(
+    (connection: Connection | Edge) => {
+      if (connection.source === connection.target) return false;
+
+      const currentEdges = getEdges()
+
+      const isDuplicate = currentEdges.some((edge) => {
+        const isDirect = edge.source === connection.source &&
+          edge.target === connection.target
+
+        const isReverse = edge.source === connection.target &&
+          edge.target === connection.source
+
+        return isReverse || isDirect
+      })
+
+      return !isDuplicate
+    }, [getEdges]
+  )
+
+  const onPaneContextMenu = useCallback(
+    (event: React.MouseEvent | MouseEvent) => {
+      event.preventDefault();
+      const position = screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY
+      })
+
+      const newNode = {
+        id: `node-${Date.now()}`,
+        type: 'mapNode',
+        position,
+        data: {label: "New node", level: 1},
+        origin: [0.5, 0.5] as [number, number]
+      }
+
+      setNodes((nodes) => nodes.concat(newNode))
+    },
+    [screenToFlowPosition, setNodes]
+  )
+
+
+  const onConnectStart = useCallback((event : MouseEvent | TouchEvent,
+    params: OnConnectStartParams) => {
+    connectionStartRef.current = params
+  }, [])
+
+  const onConnectEnd = useCallback((event: MouseEvent | TouchEvent)=> {
+    const target = event.target as Element;
+    const isPane = target.classList.contains('react-flow__pane');
+    
+    if (isPane && connectionStartRef.current) {
+      const {clientX, clientY} = 'changedTouches' in event ? 
+        event.changedTouches[0] : event;
+
+      const position = screenToFlowPosition({
+        x: clientX,
+        y: clientY
+      })
+
+      const newNodeId = `node-${Date.now()}`;
+      const newNode : Node = {
+        id: newNodeId,
+        type: "mapNode",
+        position,
+        data: {label: "New Node", level: 2},
+        origin: [0.5, 0.5] as [number, number]
+      }
+
+      const {nodeId, handleType, handleId} = connectionStartRef.current
+
+      const endHandleId = idTranslate[handleId as string];
+
+
+
+      const newEdge : Edge = {
+        id: `e-${newNodeId}-${nodeId}`,
+        source: handleType === 'source' ? nodeId! : newNodeId,
+        target: handleType === 'source' ? newNodeId : nodeId!,
+        sourceHandle: handleType === 'source' ? handleId : endHandleId,
+        targetHandle: handleType === 'target' ? handleId : endHandleId,
+        type: 'bezier'
+      }
+
+      setNodes((nodes) => nodes.concat(newNode))
+      setEdges((edges) => edges.concat(newEdge))
+    }
+  
+  }, [screenToFlowPosition, setNodes, setEdges])
+
+  return {
+    nodes, edges, nodeTypes, onNodesChange, onEdgesChange, onConnect,
+    onPaneContextMenu, isValidConnection, onConnectEnd, onConnectStart
+  }
+}
--- src/app/map-area/page.tsx
+++ src/app/map-area/page.tsx
@@ -1,162 +1,31 @@
 "use client"
 
 import Zoomer from "./components/Zoomer";
-import { applyNodeChanges, Edge, NodeChange, ReactFlow, Node, EdgeChange, applyEdgeChanges, addEdge, Connection, Background , ConnectionMode, useReactFlow, ReactFlowProvider, OnConnectStartParams} from "@xyflow/react";
-import { useCallback, useRef, useState } from "react";
+import { ReactFlow, Background , ConnectionMode,ReactFlowProvider,} from "@xyflow/react";
 import '@xyflow/react/dist/style.css';
-import MapNode from "./components/MapNode";
-
-const initialNodes : Node[] = [];
-const initialEdges : Edge[] = [];
-
-const nodeTypes = {
-  mapNode: MapNode,
-}
+import { useMapLogic } from "./hooks/useMapLogic"; 
 
 function MapFlow() {
-  const [nodes, setNodes] = useState<Node[]>(initialNodes);
-  const [edges, setEdges] = useState<Edge[]>(initialEdges);
 
-  const {screenToFlowPosition, getEdges} = useReactFlow()
-  
-  const connectionStartRef = useRef<OnConnectStartParams>(null)
+  const mapLogic = useMapLogic();
 
-  const onNodesChange = useCallback( 
-    (changes: NodeChange[]) => setNodes((nodesSnapshot) => 
-      applyNodeChanges(changes,nodesSnapshot)), []
-  );
-
-  const onEdgesChange = useCallback(
-    (changes:  EdgeChange[]) => setEdges((edgesSnapshot) =>
-      applyEdgeChanges(changes, edgesSnapshot)), []
-  );
-
-  const onConnect = useCallback(
-    (params: Edge | Connection) => setEdges((edgesSnapshot) => 
-      addEdge(params, edgesSnapshot)), []
-  );
-
-  const isValidConnection = useCallback(
-    (connection: Connection | Edge) => {
-      if (connection.source === connection.target) return false;
-
-      const currentEdges = getEdges()
-
-      const isDuplicate = currentEdges.some((edge) => {
-        const isDirect = edge.source === connection.source &&
-          edge.target === connection.target
-
-        const isReverse = edge.source === connection.target &&
-          edge.target === connection.source
-
-        return isReverse || isDirect
-      })
-
-      return !isDuplicate
-    }, [getEdges]
-  )
-
-  const onPaneContextMenu = useCallback(
-    (event: React.MouseEvent | MouseEvent) => {
-      event.preventDefault();
-      const position = screenToFlowPosition({
-        x: event.clientX,
-        y: event.clientY
-      })
-
-      const newNode = {
-        id: `node-${Date.now()}`,
-        type: 'mapNode',
-        position,
-        data: {label: "New node", level: 1},
-        origin: [0.5, 0.5] as [number, number]
-      }
-
-      setNodes((nodes) => nodes.concat(newNode))
-    },
-    [screenToFlowPosition, setNodes]
-  )
-
-
-  const onConnectStart = useCallback((event : MouseEvent | TouchEvent,
-    params: OnConnectStartParams) => {
-    connectionStartRef.current = params
-  }, [])
-
-  const onConnectEnd = useCallback((event: MouseEvent | TouchEvent)=> {
-    const target = event.target as Element;
-    const isPane = target.classList.contains('react-flow__pane');
-    
-    if (isPane && connectionStartRef.current) {
-      const {clientX, clientY} = 'changedTouches' in event ? 
-        event.changedTouches[0] : event;
-
-      const position = screenToFlowPosition({
-        x: clientX,
-        y: clientY
-      })
-
-      const newNodeId = `node-${Date.now()}`;
-      const newNode : Node = {
-        id: newNodeId,
-        type: "mapNode",
-        position,
-        data: {label: "New Node", level: 2},
-        origin: [0.5, 0.5] as [number, number]
-      }
-
-      const {nodeId, handleType, handleId} = connectionStartRef.current
-      
-      const idTranslate: Record<string, string> = {
-        "rtc": "l",
-        "r": "l",
-        "rbc": "l",
-        "bml": "t",
-        "bmr": "t",
-        "b": "t",
-        "ltc": "r",
-        "l": "r",
-        "lbc": "r",
-        "tml": "b",
-        "tmr": "b",
-        "t": "b"
-      }
-
-      const endHandleId = idTranslate[handleId as string];
-
-
-
-      const newEdge : Edge = {
-        id: `e-${newNodeId}-${nodeId}`,
-        source: handleType === 'source' ? nodeId! : newNodeId,
-        target: handleType === 'source' ? newNodeId : nodeId!,
-        sourceHandle: handleType === 'source' ? handleId : endHandleId,
-        targetHandle: handleType === 'target' ? handleId : endHandleId,
-        type: 'bezier'
-      }
-
-      setNodes((nodes) => nodes.concat(newNode))
-      setEdges((edges) => edges.concat(newEdge))
-    }
-  
-  }, [screenToFlowPosition, setNodes, setEdges])
   return (
       <div className="w-[100vw] h-[90vh]">
         <ReactFlow 
-          nodes={nodes} 
-          edges={edges}
-          nodeTypes={nodeTypes}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-          onConnect={onConnect}
+          nodes={mapLogic.nodes} 
+          edges={mapLogic.edges}
+          nodeTypes={mapLogic.nodeTypes}
+          onNodesChange={mapLogic.onNodesChange}
+          onEdgesChange={mapLogic.onEdgesChange}
+          onConnect={mapLogic.onConnect}
           zoomActivationKeyCode={"Ctrl"}
           deleteKeyCode={"Delete"}
           connectionMode={ConnectionMode.Loose}
           connectionRadius={20}
-          onPaneContextMenu={onPaneContextMenu}
-          isValidConnection={isValidConnection}
-          onConnectStart={onConnectStart}
-          onConnectEnd={onConnectEnd}
+          onPaneContextMenu={mapLogic.onPaneContextMenu}
+          isValidConnection={mapLogic.isValidConnection}
+          onConnectStart={mapLogic.onConnectStart}
+          onConnectEnd={mapLogic.onConnectEnd}
           defaultEdgeOptions={{
             type: "bezier",
             
